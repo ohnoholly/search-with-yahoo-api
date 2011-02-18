28ec3333b1c7b795e51a28e670f24f98ef0e588a
ltazmin <ltazmin@da2dca56-5c88-fef5-41ca-42149291cd56>
2011-02-18 05:14:07 +0000

    Added more functions to YahooTop10Results, including asking for relevance judgment and sanitizing output.

--- trunk/Project1/src/YahooTop10Results.java
+++ trunk/Project1/src/YahooTop10Results.java
@@ -1,13 +1,17 @@
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.regex.*;
 
 public class YahooTop10Results {
 	
 	ArrayList<ResultNode> _arr = null;	// Array of top 10 results
-
+	int count = 0;
+	float desiredPrec = 0;
+	String originalQuery = null;
+	
 	/**
 	 * Constructor
 	 */
@@ -19,8 +23,10 @@ public class YahooTop10Results {
 	 * Constructor with result data
 	 * @param res A string of data in JSON format
 	 */
-	public YahooTop10Results(String res) {
+	public YahooTop10Results(String res, String query, float prec) {
 		_arr = new ArrayList<ResultNode>();
+		originalQuery = query;
+		desiredPrec = prec;
 		buildArray(res);
 	}
 	
@@ -33,8 +39,8 @@ public class YahooTop10Results {
 		Scanner scan = new Scanner(res);
 		
 		// Parse the no of results
-		String strcount = scan.findInLine("\"count\":\"(\\w+)\"");
-		int count = Integer.parseInt(scan.match().group(1));
+		scan.findInLine("\"count\":\"(\\w+)\"");
+		count = Integer.parseInt(scan.match().group(1));
 		
 		// Parse the results abstract, title and url
 		String matchRes = "\"abstract\":\"(.*?)\",.*?\"title\":\"(.*?)\",\"url\":\"(.*?)\"";
@@ -55,25 +61,83 @@ public class YahooTop10Results {
 			_arr.add(node);
 		}
 		
-		listResults(count);
-		
 		scan.close();
 	}
 	
-	public void listResults(int count) {
+	/**
+	 * Prints the results and prompts for relevance judgment
+	 * @param count No of results
+	 */
+	public void listResults() {
 		
 		System.out.println("Total no of results : "+count);
 
 		System.out.println("Yahoo! Search Results:");
 		System.out.println("======================");
 		
+		Scanner in = new Scanner(System.in);
+		
+		// Iterate over the results
 		int i = 0;
 		for (ResultNode node : _arr) {
 			System.out.println("Result "+ ++i);
 			System.out.println(node);
-			// @@@ Add code to ask user if relevant
+			// Ask user if the result is relevant
+			getRelevance(in, node);
 		}
 		
+		in.close();
+		
+		getFeedback(originalQuery); // @@@ Change when query is altered
+	}
+	
+	/**
+	 * Prompts user if result is relevant
+	 * @param in Scanner for getting user input
+	 * @param node The result node in question
+	 */
+	public void getRelevance(Scanner in, ResultNode node) {
+		System.out.println("Relevant (Y/N)?");
+		String rel = in.next("[YyNn]"); // @@@ Need more elegant exception handling here
+		
+		// Set the node's relevance
+		node.isRelevant(rel);
+	}
+	
+	/**
+	 * Calculates the precision score of results in array
+	 * @return a precision score
+	 */
+	public float getResultPrec() {
+		float prec = 0;
+		
+		// Add up the number of relevant nodes
+		for (ResultNode node : _arr) {
+			if (node.relevant == true)
+				prec++;
+		}
+		
+		// Divide by 10 to get score
+		return prec / 10;
+	}
+	
+	/**
+	 * Prints feedback summary after one pass
+	 * @param query String containing original query
+	 */
+	public void getFeedback(String query) {
+		
+		System.out.println("======================");
+		System.out.println("FEEDBACK SUMMARY");
+		
+		System.out.println("Query "+query);
+		System.out.println("Precision "+getResultPrec());
+		
+		// Print this if the desired precision score is reached
+		if (getResultPrec() >= desiredPrec)
+			System.out.println("Desired precision reached, done");
+		
+		// @@@ what happens if not reached...
 	}
 	
 	/**
@@ -95,10 +159,9 @@ public class YahooTop10Results {
 		 * @param url Result URL
 		 */
 		public ResultNode (String summary, String title, String url) {
-			// @@@ Need to sanitize the strings to remove HTML code
-			this.title = title;
-			this.url = url;
-			this.summary = summary;
+			this.title = trim(title);
+			this.url = trim(url);
+			this.summary = trim(summary);
 		}
 		
 		/**
@@ -110,18 +173,31 @@ public class YahooTop10Results {
 		}
 		
 		/**
-		 * Accepts a string with "N" or "Y" to alter node's relevance
+		 * Sets the relevance of a result
 		 * @param bool a String that is "N" or "Y"
 		 */
 		public void isRelevant (String bool) {
-			
 			if (bool.equalsIgnoreCase("Y"))
 				relevant = true;
 			else
 				relevant = false;
-			
 		}
 		
+	}
+	
+	/**
+	 * Removes HTML code and replaces escaped characters
+	 * @param str A string from Yahoo
+	 * @return A clean string
+	 */
+	public static String trim(String str) {
+		String newStr = null;
+		
+		newStr = str.replaceAll("\\<.*?b\\>", "");
+		newStr = newStr.replaceAll("\\\\/", "/");
+		newStr = newStr.replaceAll("\\\\\"", "\"");
+
+		return newStr;
 	}
 	
 }
